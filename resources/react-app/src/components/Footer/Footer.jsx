c709e8cd8a230ca6660434b9a9e458a4db6065cb
Emirhan Çöpoğlu <copogluemirhan@outlook.com>
2024-06-25 14:29:56 +0300

    footer responsive design done.

--- resources/react-app/src/components/Footer/Footer.jsx
+++ resources/react-app/src/components/Footer/Footer.jsx
@@ -16,9 +16,9 @@ const Footer = ({ data }) => {
     return (
         <div className="w-full h-full flex flex-col  border-t bg-[#363384]">
             <div className="pt-24 pb-32 flex flex-col w-full max-sm:px-0 max-sm:py-8 max-sm:flex-col max-lg:px-0 max-lg:pb-12 max-xl:px-8">
-                <div className="flex flex-row w-full container mx-auto gap-40 max-md:flex-col max-md:gap-8 max-xl:gap-0">
-                    <div className="flex flex-col w-1/3 px-4 max-sm:w-full max-md:items-center">
-                        <div className="w-72 object-contain">
+                <div className="flex flex-row w-full container mx-auto gap-40 max-md:flex-col max-md:gap-8 max-md:items-center max-xl:gap-0 max-2xl:gap-20">
+                    <div className="flex flex-col w-1/3 px-4 max-sm:w-full max-md:items-center max-2xl:justify-center">
+                        <div className="w-72 object-contain max-xl:w-full ">
                             <img
                                 src={FooterLogo}
                                 alt="Vira Gas"
@@ -26,9 +26,9 @@ const Footer = ({ data }) => {
                             ></img>
                         </div>
                     </div>
-                    <div className="flex flex-row w-full gap-40 max-md:flex-col max-md:gap-0 max-xl:gap-0 max-xl:flex-col">
-                        <div className="flex flex-col px-4 w-full max-sm:w-full max-sm:py-8 justify-center items-center gap-4">
-                            <ul className="list-none flex flex-row gap-10 text-white font-semibold max-md:flex-wrap max-md:gap-3">
+                    <div className="flex flex-row w-full gap-40 max-md:flex-col max-md:gap-0 max-xl:flex-col max-xl:gap-12 max-2xl:gap-12">
+                        <div className="flex flex-col px-4 w-full justify-center items-center gap-4  max-sm:w-full max-sm:py-8">
+                            <ul className="list-none flex flex-row gap-10 text-white font-semibold max-md:flex-wrap max-md:justify-center max-md:gap-3 ">
                                 <li className=" text-lg max-md:text-[1rem]">
                                     <Link
                                         to="/kurumsal"
@@ -70,7 +70,7 @@ const Footer = ({ data }) => {
                                     </Link>
                                 </li>
                             </ul>
-                            <ul className="list-none flex flex-row text-white gap-10 font-semibold max-md:flex-wrap max-md:gap-4">
+                            <ul className="list-none flex flex-row text-white gap-10 font-semibold max-md:flex-wrap max-md:justify-center max-md:gap-4">
                                 <li className=" text-lg max-md:text-[1rem]">
                                     <Link
                                         to="/kurumsal"
@@ -107,7 +107,7 @@ const Footer = ({ data }) => {
                         </div>
                         <div className="flex flex-col px-4 w-full max-sm:pb-8  justify-center items-center">
                             <ul className="list-none text-white flex flex-col gap-4">
-                                <li className=" text-lg flex flex-row items-center gap-2 max-md:text-center ">
+                                <li className=" text-lg flex flex-row items-center gap-2 max-md:text-center max-xl:text-center">
                                     <p className="max-md:text-[1rem]">
                                         Çalı Sanayi Bölgesi Karaali Mah. 7.
                                         Sokak No:19 Çalı /Nilüfer /Bursa
@@ -115,7 +115,7 @@ const Footer = ({ data }) => {
                                     </p>
                                 </li>
 
-                                <li className=" text-lg flex flex-row items-center gap-2 max-md:justify-center ">
+                                <li className=" text-lg flex flex-row items-center gap-2 max-md:justify-center max-xl:justify-center">
                                     <MdOutlinePhoneInTalk size={24} />
                                     <a
                                         href="tel:+902244824427"
@@ -124,16 +124,16 @@ const Footer = ({ data }) => {
                                         (+90) 224 482 44 27
                                     </a>
                                 </li>
-                                <li className=" text-lg flex flex-row items-center gap-2 max-md:justify-center">
+                                <li className=" text-lg flex flex-row items-center gap-2 max-md:justify-center max-xl:justify-center">
                                     <MdOutlineFax size={24} />
                                     <a
                                         href="fax:+902244824427"
                                         className="max-md:text-[1rem]"
                                     >
-                                        (+90)224 482 44 27
+                                        (+90) 224 482 44 27
                                     </a>
                                 </li>
-                                <li className=" text-lg flex flex-row items-center gap-2 max-md:justify-center ">
+                                <li className=" text-lg flex flex-row items-center gap-2 max-md:justify-center max-xl:justify-center">
                                     <CiMail size={24} />
                                     <a
                                         href="mailto:sales@viragas.com"
@@ -147,9 +147,9 @@ const Footer = ({ data }) => {
                     </div>
                 </div>
             </div>
-            <div className="border-t flex flex-row justify-between container mx-auto pt-4 pb-4 ">
+            <div className="border-t flex flex-row justify-between container mx-auto py-4 max-xl:px-4">
                 <div>
-                    <p className="text-white font-light max-md:text-[0.9rem] max-md:px-4">
+                    <p className="text-white font-light max-md:text-[0.9rem]">
                         © 2024 VİRA Gas Springs, tüm hakları saklıdır.
                     </p>
                 </div>
